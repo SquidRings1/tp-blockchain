48da8f850f7dc72d866e0b5e81a3157f94e62d26
Chaudhry Haris <haris-ul-haq.chaudhry@epita.fr>
2025-12-21 21:17:16 +0100

    feat: update token name in theKoken contract and refactor VestingWalletTest setup

--- test/VestingWallet.t.sol
+++ test/VestingWallet.t.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {VestingWallet} from "../src/VestingWallet.sol";
 
 contract theKoken is ERC20 {
-    constructor() ERC20("the TOKEN", "TEST") {
+    constructor() ERC20 ("xwingtoken", "XT") {
         _mint(msg.sender, 1000000 * 10**18);
     }
 }
@@ -15,8 +15,25 @@ contract VestingWalletTest is Test {
     VestingWallet public vesting;
     theKoken public token;
 
-    constructor() {
+    address public owner;
+    address public beneficiary1;
+
+    uint256 constant AMOUNT = 1;
+    uint256 constant CLIFF = 30 days;
+    uint256 constant DURATION = 365 days;
+
+    function setUp() public {
+        owner = address(this);
+        beneficiary1 = address(0x1);
+
         token = new theKoken();
         vesting = new VestingWallet(address(token));
+        token.approve(address(vesting), AMOUNT);
+    }
+
+    function testCreateVesting() public {
+        vesting.createVestingSchedule(beneficiary1, AMOUNT, CLIFF, DURATION);
+        (address beneficiary, uint256 cliff, uint256 duration, uint256 totalAmount, uint256 releasedAmount) = vesting.vestingSchedules(beneficiary1);
+        assertEq(totalAmount, AMOUNT);
     }
 }
